e0802f01095d3ca44f7179dd913facbe77970576
simple2ad <112989539+simple2ad@users.noreply.github.com>
2025-10-22 15:45:40 -0400

    Configure with actual Neon database credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL || process.env.DATABASE_URL!,
   },
 } satisfies Config;
